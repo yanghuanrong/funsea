f5753e83b8f1c235705598ad254459b1d69c9ac1
yanghuanrong <1176989241@qq.com>
2018-01-13 23:21:34 +0800

    fix bug

--- js/main.js
+++ js/main.js
@@ -2,20 +2,20 @@ jQuery(document).ready(function($) {
 
 	'use strict';
 
-    // var owl = $("#owl-portfolio");
-    //
-    //   owl.owlCarousel({
-    //
-    //     pagination : true,
-    //     paginationNumbers: false,
-    //     autoPlay: 6000, //Set AutoPlay to 3 seconds
-    //     items : 4, //10 items above 1000px browser width
-    //     itemsDesktop : [1000,4], //5 items between 1000px and 901px
-    //     itemsDesktopSmall : [900,3], // betweem 900px and 601px
-    //     itemsTablet: [600,2], //2 items between 600 and 0
-    //     itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
-    //
-    // });
+    var owl = $("#owl-portfolio");
+
+      owl.owlCarousel({
+
+        pagination : true,
+        paginationNumbers: false,
+        autoPlay: 6000, //Set AutoPlay to 3 seconds
+        items : 4, //10 items above 1000px browser width
+        itemsDesktop : [1000,4], //5 items between 1000px and 901px
+        itemsDesktopSmall : [900,3], // betweem 900px and 601px
+        itemsTablet: [600,2], //2 items between 600 and 0
+        itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+
+    });
 
 
     $('.tabgroup > div').hide();
